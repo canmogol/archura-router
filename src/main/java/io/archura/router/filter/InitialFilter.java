de099da5676ac5da9839f7b942418088b53ab616
Ali Can Mogol <amogol@ymor.com>
2023-01-29 17:57:21 +0100

    Add find current path method.

--- src/main/java/io/archura/router/filter/InitialFilter.java
+++ src/main/java/io/archura/router/filter/InitialFilter.java
@@ -9,10 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 @Slf4j
 @Component
@@ -21,6 +21,10 @@ public class InitialFilter implements Filter {
 
     private static final String ARCHURA_DOMAIN = "archura.domain";
     private static final String DEFAULT_DOMAIN = "default";
+    private static final String ARCHURA_TENANT = "archura.tenant";
+    private static final String DEFAULT_TENANT = "default";
+    private static final String ARCHURA_DOMAIN_NOT_FOUND_URL = "archura.domain.not-found.url";
+    private static final String HTTP_METHOD_GET = "GET";
     private final GlobalConfiguration globalConfiguration;
     private final FilterFactory filterFactory;
 
@@ -53,8 +57,18 @@ public class InitialFilter implements Filter {
                     .forEach((name, configuration) -> runFilter(httpServletRequest, httpServletResponse, name, configuration));
 
             // tenant pre-filters
+            if (isNull(httpServletRequest.getAttribute(ARCHURA_TENANT))) {
+                httpServletRequest.setAttribute(ARCHURA_TENANT, DEFAULT_TENANT);
+            }
+            final String tenant = String.valueOf(httpServletRequest.getAttribute(ARCHURA_TENANT));
+            final GlobalConfiguration.TenantConfiguration tenantConfiguration = domainConfiguration.getTenants().get(tenant);
+            tenantConfiguration.getTenantPreFilters()
+                    .forEach((name, configuration) -> runFilter(httpServletRequest, httpServletResponse, name, configuration));
 
             // route pre-filters
+            final GlobalConfiguration.RouteConfiguration currentRoute = findCurrentRoute(httpServletRequest, domainConfiguration, tenantConfiguration);
+            currentRoute.getRoutePreFilters()
+                    .forEach((name, configuration) -> runFilter(httpServletRequest, httpServletResponse, name, configuration));
 
             // send downstream request
             // Routing
@@ -84,6 +98,54 @@ public class InitialFilter implements Filter {
         } else {
             filterChain.doFilter(servletRequest, servletResponse);
         }
+    }
+
+    private GlobalConfiguration.RouteConfiguration findCurrentRoute(
+            final HttpServletRequest httpServletRequest,
+            final GlobalConfiguration.DomainConfiguration domainConfiguration,
+            final GlobalConfiguration.TenantConfiguration tenantConfiguration
+    ) {
+        final String method = httpServletRequest.getMethod();
+
+        final List<GlobalConfiguration.RouteConfiguration> routeConfigurations = tenantConfiguration.getMethodRoutes().get(method);
+        if (nonNull(routeConfigurations)) {
+            // TODO: return the first matching route
+        }
+
+        final List<GlobalConfiguration.RouteConfiguration> catchAllRoutes = tenantConfiguration.getMethodRoutes().get("*");
+        if (nonNull(catchAllRoutes)) {
+            // TODO: return the first matching route
+        }
+
+        // return not found route
+        final String notFoundUrl = nonNull(domainConfiguration.getParameters().get(ARCHURA_DOMAIN_NOT_FOUND_URL))
+                ? domainConfiguration.getParameters().get(ARCHURA_DOMAIN_NOT_FOUND_URL) : "/not-found";
+
+        final GlobalConfiguration.RouteConfiguration notFoundRoute = new GlobalConfiguration.RouteConfiguration();
+        final GlobalConfiguration.MapConfiguration notFoundMap = createNotFoundMap(httpServletRequest, method, notFoundUrl);
+        notFoundRoute.setMapConfiguration(notFoundMap);
+        return notFoundRoute;
+    }
+
+    private GlobalConfiguration.MapConfiguration createNotFoundMap(
+            final HttpServletRequest httpServletRequest,
+            final String method,
+            final String notFoundUrl
+    ) {
+        final GlobalConfiguration.MapConfiguration notFoundMap = new GlobalConfiguration.MapConfiguration();
+        notFoundMap.setUrl(notFoundUrl);
+        notFoundMap.setMethodMap(Map.of(method, HTTP_METHOD_GET));
+        notFoundMap.setHeaders(getRequestHeaders(httpServletRequest));
+        return notFoundMap;
+    }
+
+    private Map<String, String> getRequestHeaders(final HttpServletRequest httpServletRequest) {
+        Map<String, String> headers = new HashMap<>();
+        for (String headerName : Collections.list(httpServletRequest.getHeaderNames())) {
+            String headerValue = httpServletRequest.getHeader(headerName);
+            headers.put(headerName, headerValue);
+        }
+        return headers;
     }
 
     private void runFilter(
