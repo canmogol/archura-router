3681e8c39d3cc370ae3d925c8d4ea1460d86a652
Can MOGOL <canmogol@gmail.com>
2023-02-14 16:41:42 +0100

    Add a test server that returns current date.

---
+++ server-mock/test-server.js
@@ -0,0 +1,10 @@
+const {createServer} = require('http');
+const fs = require('fs');
+
+const server = createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(new Date().toString());
+    res.end();
+});
+
+server.listen(9020);
