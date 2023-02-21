c56176e94cba3118b321bdf49dd299b17f5ec6d5
Can MOGOL <canmogol@gmail.com>
2023-02-21 14:45:56 +0100

    Log request URI.

--- server-mock/test-server.js
+++ server-mock/test-server.js
@@ -2,6 +2,7 @@ const {createServer} = require('http');
 const fs = require('fs');
 
 const server = createServer(function (req, res) {
+    console.log(req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(new Date().toString());
     res.end();
